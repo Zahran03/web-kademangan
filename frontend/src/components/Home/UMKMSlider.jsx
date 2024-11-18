3896d81f41fbcdba2eac5cfc00b6b2fcaaadeb37
Zahran03 <zalfarizi502@gmail.com>
2024-11-18 21:53:07 +0700

    feat: fetching data to umkm section

--- frontend/src/components/Home/UMKMSlider.jsx
+++ frontend/src/components/Home/UMKMSlider.jsx
@@ -1,7 +1,23 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { useEffect, useState } from "react";
 
 const UMKMSlider = () => {
+  const [umkmData, setUmkmData] = useState([]);
+
+  useEffect(() => {
+    const fecthData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/UMKM");
+        const data = await res.json();
+        setUmkmData(data);
+      } catch (error) {
+        throw new Error("error doing fetch");
+      }
+    };
+    fecthData();
+  }, []);
+
   return (
     <Swiper
       slidesPerView={"auto"}
@@ -10,9 +26,14 @@ const UMKMSlider = () => {
       centeredSlides={true}
       className="overflow-hidden flex items-center justify-center w-full umkm-swiper"
     >
-      <SwiperSlide className="max-w-[300px] swiper-slider text-center h-full flex items-center justify-center m-0 transition-transform aspect-square bg-white text-red-500">
-        <div className="flex items-center justify-center w-full h-full">1</div>
-      </SwiperSlide>
+      {umkmData.map((umkm) => (
+        <SwiperSlide className="max-w-[300px] swiper-slider text-center h-full flex items-center justify-center m-0 transition-transform aspect-square bg-white text-red-500">
+          <img
+            src={`http://localhost:3000/uploads/${umkm.gambar}`}
+            className="flex items-center justify-center w-full h-full object-cover object-center"
+          />
+        </SwiperSlide>
+      ))}
       <SwiperSlide className="max-w-[300px] swiper-slider text-center h-full flex items-center justify-center m-0 transition-transform aspect-square bg-white text-red-500">
         <div className="flex items-center justify-center w-full h-full">2</div>
       </SwiperSlide>
